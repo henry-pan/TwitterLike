c30b62631c1907fecd47c33aa109165eb67fb7c0
Henry <henry-pan@users.noreply.github.com>
2021-04-18 01:14:40 -0700

    Add frontend

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>TwitterLike: A Twitter Clone</h1>
+        <footer>
+          Copyright &copy; 2021 Henry Pan
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
